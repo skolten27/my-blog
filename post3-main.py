4760da6c2aea1ba90b71a53564edb84c303e355a
Kolten Smith <skolten27@gmail.com>
2024-12-03 19:19:28 -0700

    Improving visual appeal

--- post3-main.py
+++ post3-main.py
@@ -19,9 +19,15 @@ def load_name_data():
 
 data = load_name_data()
 
-st.title("Kolten's First Streamlit app!")
+st.markdown("""
+# Analyzing Earnings and Market Trends 📊
+Welcome to the earnings and market trends analysis app! 
+Select a stock ticker and adjust the filters to explore trading volume, earnings surprises, and stock price changes.
+""")
 
 with st.sidebar:
+    st.header("Filter Options")
+    st.write("Customize the filters below to analyze specific trends.")
     stock_ticker = st.radio('Ticker Symbol', ['WEN', 'TSLA', 'AAPL', 'GOOG'])
     year_input = st.slider("Year", min_value=1999, max_value=2024, value=2024)
 
@@ -29,15 +35,21 @@ with st.sidebar:
 tab1, tab2, tab3 = st.tabs(['Trading Volume', 'Surprise by Year', 'Surprise vs Stock Price Change'])
 
 with tab1: 
+    st.subheader("📈 Trading Volume Analysis")
+    st.write("Analyze the average trading volume for your selected stock. You can change the selected stock using the Ticker Symbol radio on the left")
     stock_data = data[data['symbol']==stock_ticker].copy()
     fig = plot_average_trading_volume(stock_data, stock = stock_ticker)
     st.plotly_chart(fig)
 
 with tab2:
+    st.subheader("📊 Surprise Percentage Anlysis")
+    st.write("Analyze the density of surprise in earnings reports. You can change the selected year using the Year slider on the left")
     fig2 = plot_density_surprise_percentage_by_year(data,year = year_input)
     st.plotly_chart(fig2)
 
-with tab3:
+with tab3:   
+    st.subheader("📉 Surprise vs. Stock Price Change Analysis")
+    st.write("Analyze the stock price change percentage and earnings suprise percentage for your selected stock. You can change the selected stock using the Ticker Symbol radio on the left. You can adjust the minimum and maximum earnings surprise percentage using the text boxes above to be able to focus on smaller portions and identify trends.")
     min_surprise = st.number_input('Minimum Surprise Percentage', value=-1000)
     max_surprise = st.number_input('Maximum Surprise Percentage', value=5000)
     fig3 = plot_surprise_vs_price_change(data, symbol=stock_ticker, min_surprise=min_surprise, max_surprise=max_surprise)
