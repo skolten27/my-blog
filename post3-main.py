f7b5b469cedfc482253fa79ca4e7fca8511de0ff
Kolten Smith <skolten27@gmail.com>
2024-12-03 17:17:59 -0700

    not quite

--- post3-main.py
+++ post3-main.py
@@ -32,7 +32,7 @@ tab1, tab2, tab3 = st.tabs(['Names', 'Year', 'Trends'])
 
 with tab1: 
     stock_data = data[data['symbol']==stock_ticker].copy()
-    fig = plot_trading_volume_around_earnings(stock_data, stock = stock_ticker)
+    fig = plot_average_trading_volume(stock_data, stock = stock_ticker)
     st.plotly_chart(fig)
 
 with tab2:
